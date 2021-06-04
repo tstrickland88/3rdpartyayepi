2f8e6161ef27a2808dffb9e24b0e8476b23b3bb0
tstrickland88 <77135762+tstrickland88@users.noreply.github.com>
2021-06-03 20:56:15 -0700

    added color and timestamp

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,28 @@ $("#9").val(localStorage.getItem("9"))
 $("#10").val(localStorage.getItem("10"))
 $("#11").val(localStorage.getItem("11"))
 $("#12").val(localStorage.getItem("12"))
-$("#1").val(localStorage.getItem("1"))
-$("#2").val(localStorage.getItem("2"))
-$("#3").val(localStorage.getItem("3"))
-$("#4").val(localStorage.getItem("4"))
-$("#5").val(localStorage.getItem("5"))
+$("#13").val(localStorage.getItem("13"))
+$("#14").val(localStorage.getItem("14"))
+$("#15").val(localStorage.getItem("15"))
+$("#16").val(localStorage.getItem("16"))
+$("#17").val(localStorage.getItem("17"))
 
+var currTime=moment().format("dddd MMMM YYYY hh:mm:ss")
+$("#currentDay").text(currTime)
 
+$("textarea").each(function(){
+var temp=$(this).attr('id')
+console.log(temp)
+var nowTime= moment().hours()
+if(temp<nowTime){
+$(this).addClass("past")
+} else if(temp==nowTime){
+$(this).addClass("present")
+} else{
+$(this).addClass("future")
+
+}
+})
 $(document).ready(function(){
 
 $(".save").on("click",function(){
